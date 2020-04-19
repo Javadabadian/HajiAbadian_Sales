ac0237e524e6b87968e19ef9c123ab5cea5591b1
Javad <haj19183@spengergasse.at>
2020-04-19 20:37:38 +0200

    Add LastName as String

--- src/src/Customer.java
+++ src/src/Customer.java
@@ -2,6 +2,28 @@ package src;
 
 public class Customer {
 private int customerID;
+public Customer(int customerID, String firstName, double credit, String lastName) {
+	super();
+	this.customerID = customerID;
+	this.firstName = firstName;
+	this.credit = credit;
+	this.lastName = lastName;
+}
+private String firstName;
+private double credit;
+private String lastName; 
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+public double getCredit() {
+	return credit;
+}
+public void setCredit(double credit) {
+	this.credit = credit;
+}
 public int getCustomerID() {
 	return customerID;
 }
@@ -14,11 +36,6 @@ public String getFirstName() {
 public void setFirstName(String firstName) {
 	this.firstName = firstName;
 }
-private String firstName;
-public Customer(int customerID, String firstName) {
-	super();
-	this.customerID = customerID;
-	this.firstName = firstName;
-}
+
 
 }
---
+++ src/src/Customers.java
@@ -0,0 +1,8 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Customers {
+	ArrayList<Customer> customList = new ArrayList<Customer>( );
+
+}
